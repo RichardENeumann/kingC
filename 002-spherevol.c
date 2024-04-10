106ab2f6b2a271e898b9558434114f00c67d8f40
RichardENeumann <RichardNeumann@googlemail.com>
2024-04-10 21:05:13 +0200

    Expand sphere program to accept user input

--- 002-spherevol.c
+++ 002-spherevol.c
@@ -3,9 +3,13 @@
 #define PI 3.14156f
 
 int main(void) {
-  int radius = 10;
+  float radius = 0;
 
-  printf("The volume of a 10m radius sphere is %.2f cubic metres.\n", ((4.0f/3.0f) * PI * radius * radius * radius));
+  puts("This program will calculate the volume of a sphere by its given radius.");
+  puts("Please enter the radius of the sphere in metres:");
+  scanf("%f", &radius);
+
+  printf("The volume of a %.2fm radius sphere is %.2f cubic metres.\n", radius, ((4.0f/3.0f) * PI * radius * radius * radius));
 
   return 0;
 }
