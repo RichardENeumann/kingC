08f5024b7642a133198b330812461d1ad54bc2b7
RichardENeumann <RichardNeumann@googlemail.com>
2024-05-07 15:30:32 +0200

    Add project 602

---
+++ 6/602-gcd.c
@@ -0,0 +1,21 @@
+// Asks a user to enter two integers then displays their greatest common divisor
+
+#include <stdio.h>
+
+int main(void) {
+  int m, n, remainder;
+
+  printf("Enter two integers: ");
+  scanf("%d %d", &m, &n);
+
+  while (n > 0) {
+    remainder = m % n;
+    m = n; 
+    n = remainder;
+  }
+
+  printf("Greatest common divisor: %d\n", m);
+
+
+  return 0;
+}
